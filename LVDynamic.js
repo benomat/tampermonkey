85b28b40ca84daa840a44c94cd850dd28dfba166
benomat <71938735+benomat@users.noreply.github.com>
2024-07-02 12:35:20 +0200

    Dynamic Linkvertise bypass

---
+++ LVDynamic.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Dynamic Linkvertise Bypass
+// @homepageURL  https://discord.gg/gUMYGXqPPw
+// @namespace    https://github.com/benomat/tampermonkey
+// @version      1.0
+// @description  Bypasses moonsec and many keysystems
+// @author       benomat
+// @supportURL   https://discord.gg/gUMYGXqPPw
+// @match        https://linkvertise.com/*dynamic?r=*
+// @icon         https://files.catbox.moe/kflwuj.png
+// @downloadURL https://raw.githubusercontent.com/benomat/tampermonkey/m/LVDynamic.js
+// @updateURL https://raw.githubusercontent.com/benomat/tampermonkey/m/LVDynamic.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const currentUrl = window.location.href;
+    const urlPattern = /https:\/\/linkvertise\.com\/.*dynamic\?r=([^&]*)/;
+    const match = currentUrl.match(urlPattern);
+    if (match && match[1]) {
+        const base64String = match[1];
+        try {
+            const decodedBase64 = decodeURIComponent(base64String);
+            const decodedUrl = atob(decodedBase64);
+            window.location.href = decodedUrl;
+        } catch (e) {
+            console.error('Failed to decode base64 string', e);
+        }
+    }
+})();
